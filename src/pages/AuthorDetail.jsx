351cd9ac3b52be2a4e2d276341be0f0de38835f9
bayuindrap <indraputrabayu@gmail.com>
2023-07-24 13:20:27 +0700

    detail with card

--- src/pages/AuthorDetail.jsx
+++ src/pages/AuthorDetail.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { Box, SimpleGrid, Card, CardHeader, CardBody, Text, Heading, Button, CardFooter } from '@chakra-ui/react'
 
 const AuthorDetail = () => {
     const { id } = useParams();
@@ -36,22 +37,60 @@ const AuthorDetail = () => {
         return <p>Error: {error.message}</p>;
     }
 
+    // const printData = () => {
+    //     return authorDetail.map((value, index) => {
+    //         // return <div>
+    //         //     <h3>{value.number}</h3>
+    //         //     <h3>{value.arab}</h3>
+    //         //     <h3>{value.id}</h3>
+    //         // </div>
+    //         return <Box>
+    //             <SimpleGrid spacing={4} templateColumns='repeat(auto-fill, minmax(200px, 1fr))'>
+    //                 <Card>
+    //                     <CardHeader>
+    //                         <Heading size='md'>{value.arab}</Heading>
+    //                     </CardHeader>
+    //                     <CardBody>
+    //                         <Text>{value.id}</Text>
+    //                     </CardBody>
+    //                     {/* <CardFooter>
+    //                         <Button>View here</Button>
+    //                     </CardFooter> */}
+    //                 </Card>
+    //             </SimpleGrid>
+    //         </Box>
+    //     })
+    // }
+
     const printData = () => {
-        return authorDetail.map((value, index) => {
-            return <div>
-                <h3>{value.number}</h3>
-                <h3>{value.arab}</h3>
-                <h3>{value.id}</h3>
-            </div>
-        })
-    }
+        return (
+          <SimpleGrid spacing={4} templateColumns='repeat(3, 1fr)'>
+            {authorDetail.map((value, index) => (
+              <Box key={index}>
+                <Card>
+                  <CardHeader>
+                    <Heading size='md'>{value.arab}</Heading>
+                  </CardHeader>
+                  <CardBody>
+                    <Text>{value.id}</Text>
+                  </CardBody>
+                  <CardFooter>
+                    <Button>View more..</Button>
+                  </CardFooter>
+                </Card>
+              </Box>
+            ))}
+          </SimpleGrid>
+        );
+      };
+      
 
     return (
         <div>
-            <h2 style={{color: "black"}}>Detail</h2>
-            <p style={{color: "black"}}>Author Name: {id}</p>
+            <h2 style={{ color: "black" }}>Detail</h2>
+            <p style={{ color: "black" }}>Author Name: {id}</p>
             {authorDetail && (
-                <div style={{color: "black"}}>
+                <div style={{ color: "black" }}>
                     {/* <h3 style={{color: "black"}}>{authorDetail.arab}</h3>
                     <p style={{color: "black"}}>{authorDetail.description}</p> */}
                     {printData()}
